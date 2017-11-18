d5a835f0f11742ca07b121ba6748c4c355bfa27c
CT-Clark <cody.t.clark@gmail.com>
2017-11-17 21:57:52 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
+var roleHarvester = require('role.harvester'); // Use roll for Harvester
+var roleUpgrader = require('role.upgrader');   // Use roll for Upgrader
+
 module.exports.loop = function () {
-    var creep = Game.creeps['Harvester1'];
-    var sources = creep.room.find(FIND_SOURCES);
-    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[0]);
+
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester'); // COunts the number of harvesters
+    console.log('Harvesters: ' + harvesters.length); // Prints out the number of harvesters
+    
+    // Spawns a harvester if there are ever less than 2 of them
+    if(harvesters.length < 2) {
+        var newName = 'Harvester' + Game.time;
+        console.log('Spawning new harvester: ' + newName);
+        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
+            {memory: {role: 'harvester'}});        
+    }
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
     }
 }
